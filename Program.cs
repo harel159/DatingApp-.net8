9908c44aee083dcf4971de3ab5ded082a212ff4c
Harel159 <harel.jerbi1@gmail.com>
2024-09-10 19:50:46 +0300

    End of section 3

--- Program.cs
+++ Program.cs
@@ -10,13 +10,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")).EnableSensitiveDataLogging().EnableDetailedErrors();
 });
+builder.Services.AddCors();
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4300" , "https://localhost:4300"));
 
 app.MapControllers();
 
